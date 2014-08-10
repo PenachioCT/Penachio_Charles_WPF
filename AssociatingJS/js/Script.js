7bfb59322f78003a7146e1b69b4f0e75e1bb3abc
Charles Penachio Jr <ctpenachio@fullsail.edu>
2014-08-10 12:49:48 -0400

    Casting
    Following Screencast:  Casting

--- AssociatingJS/js/Script.js
+++ AssociatingJS/js/Script.js
@@ -1,9 +1,11 @@
-//Expressions - Concatenating Strings
+//Casting
 
-var a = 6;
-var b = 7;
+var areaCode = 407;
+var firstPart = 203;
+var secPart = 4529;
+//(407) 203-4529
+var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart);
+console.log(phoneNo);
 
-var result = a + b;
-console.log(result);
 
 
