d8559d2e2f3a3c99f5a32b5dd1f761cb5bc3e1ff
Charles Penachio Jr <ctpenachio@fullsail.edu>
2014-08-10 12:30:13 -0400

    Order of Operations
    Following Screencast:  Order of Operations

--- AssociatingJS/js/Script.js
+++ AssociatingJS/js/Script.js
@@ -1,8 +1,17 @@
-//Expressions - Modulo Operator
+//Expressions - PEMDAS - The Order of Operations
 
-var remainder = 9 % 2;
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
 
+//the average is the sum of the items divided by the number of items
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+//console.log(average);
 
-console.log(remainder);
+var length = 7;
+var width = 6;
+var perimeter = length * 2 + width * 2;
+console.log(perimeter);
 
 
