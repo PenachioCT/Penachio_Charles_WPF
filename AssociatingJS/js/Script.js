9fbe54d99ec6b2735561b3b12076d012ca061893
Charles Penachio Jr <ctpenachio@fullsail.edu>
2014-08-10 10:03:47 -0400

    Defining Variables
    Following Screencast:  Defining Variables

--- AssociatingJS/js/Script.js
+++ AssociatingJS/js/Script.js
@@ -1,5 +1,11 @@
-var age;
+// Defining Variables
 
+var age;  //declaration
+age = 32;  //definition
+
+//storage container = value
+
+var yearOfBirth = 1982;  //declaring and defining on the same line
 
 
 
