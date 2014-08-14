7896d29e7120f83fe628f6ef9ceec02555be4b43
Charles Penachio Jr <ctpenachio@fullsail.edu>
2014-08-14 18:27:35 -0400

    User Variables
    using prompts to user and storing values in variables so we can use
    them to calculate later on

--- Expressions_Personal/js/Script.js
+++ Expressions_Personal/js/Script.js
@@ -10,6 +10,8 @@ Expressions - Personal
 var userName = prompt("Please enter your name: ");
 console.log("The users name is reported as: " + userName);
 alert("Hello, " + userName + ".  \nLet's calculate your monthly income and expenses.");
-
+var userIncome   = prompt("Please enter your monthly income (in dollars):");
+var userExpenses = prompt("Please enter your monthly expenses (in dollars):");
+var userPercent  = prompt("Please enter a percentage of your monthly income you would like to put into savings:");
 
 
