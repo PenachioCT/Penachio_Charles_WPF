edb7a7cf8a80349e552b5ba3737a73ab9460b99b
Charles Penachio Jr <ctpenachio@fullsail.edu>
2014-08-14 18:25:50 -0400

    Display name variable
    Displaying name to console and user alert

--- Expressions_Personal/js/Script.js
+++ Expressions_Personal/js/Script.js
@@ -8,6 +8,8 @@ Expressions - Personal
 //We will be helping someone calculate their monthly finances.
 
 var userName = prompt("Please enter your name: ");
+console.log("The users name is reported as: " + userName);
+alert("Hello, " + userName + ".  \nLet's calculate your monthly income and expenses.");
 
 
 
