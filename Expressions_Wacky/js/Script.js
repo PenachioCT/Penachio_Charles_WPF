9eee6be1d356e2c9cad4b396c12c28b50e7d43ac
Charles Penachio Jr <ctpenachio@fullsail.edu>
2014-08-14 19:42:17 -0400

    Formatting
    Formatting

--- Expressions_Wacky/js/Script.js
+++ Expressions_Wacky/js/Script.js
@@ -7,8 +7,10 @@ Assignment:  Expressions - Wacky
 
 //Array fun with Mad Libs
 var userName = prompt("Please enter your name:");
+
 console.log(userName);
 alert("Welcome " + userName + ". \n Let's have some fun with MadLibs!");
+
 var madlibArray = new Array ();
 madlibArray[0] = prompt("Please enter an Adverb \n(Describes an action - Ex: Quickly, Carefully, Silently):");
 madlibArray[1] = prompt("Please enter an Adjective \n(Describes something - Ex:  Green, Squishy, Cute):");
@@ -39,15 +41,3 @@ and helpfulness with one another. Moving forward I continue to have
 high hopes for what has yet to come.
     */
 
-/*
- adjective  describes something (Green, Squishy, Cute)
- adverb  describs an action (Quickly, Carefully, Silently)
- noun  person place or thing (Car, House, Child.)
- plural noun  people places or things (Cars, Houses, Children.)
- verb  An action (Run, Jump, Swim)
- excalamation  (Wow!, Oh!, Yuck!)
- number  (5, 223, 16.5)
- color  (Red, Green, Blue)
- name  (Bill, Fred, Lisa)
-
- */
