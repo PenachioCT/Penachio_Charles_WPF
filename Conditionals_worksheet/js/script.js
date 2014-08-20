12d73ee33084991501c53e51727038e28a8d628e
Charles Penachio Jr <ctpenachio@fullsail.edu>
2014-08-19 21:31:57 -0400

    Exercise
    Completed the Exercise Problem

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -5,3 +5,15 @@
  08/19/2014
  */
 
+//Example;  Stuff your face I
+var compWeight = 290;   //declares the competitors weight
+
+if(compWeight >= 250){
+    //determines if the competitors weight meets the requirements to qualify
+    console.log("The competitor qualifies for the heavyweight division.");
+}else{
+    //message if competitor fails to meet the requirement to qualify
+    console.log("The competitor needs to gain some weight!");
+}
+console.log"\n";    //Spacing between Example & Group 1 Exercise
+
