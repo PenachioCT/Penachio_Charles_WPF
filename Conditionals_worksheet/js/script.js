b4abfa5e27c99a10f38a094b1c8ef26cdce17cdc
Charles Penachio Jr <ctpenachio@fullsail.edu>
2014-08-19 21:37:35 -0400

    Exercise Information
    Transfer of exercise directions into script to prevent swapping back
    and forth.

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -18,3 +18,14 @@ if(compWeight >= 250){
 console.log"\n";    //Spacing between Example & Group 1 Exercise
 
 //Group 1:  Expressions w/Conditionals - Last Chance for Gas!
+var gasEff      //in mpg
+var gasGauge    //in %
+var gasCap      //in gallons
+
+if(){
+    //can you make it across the desert w/current fuel?  next gas station in 200 mi.  stop, yes or no?
+
+}
+
+console.log("Yes, you can make it without stopping for gas!");
+console.log("You only 'X' gallons of gas in your tank, better get gas now while you can!");
