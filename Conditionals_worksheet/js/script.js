a6757917b5f906dc1efbbb89d3f997bd289271d7
Charles Penachio Jr <ctpenachio@fullsail.edu>
2014-08-19 22:47:50 -0400

    Group 1 If/Else
    Implemented If/Else statement for Group 1

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -18,16 +18,20 @@ if(compWeight >= 250){
 console.log("");    //Spacing between Example & Group 1 Exercise
 
 //Group 1:  Expressions w/Conditionals - Last Chance for Gas!
-var gasEff    = 30;    //in mpg
-var gasGauge  = 50     //in %
+var gasEff    = 20;    //in mpg
+var gasGauge  = 50;    //in %
 var gasCap    = 20;    //in gallons
+var nextGas   = 200;   //miles until next gas station
 
-var gasRemain = gasCap * (gasGauge/100);
-console.log(gasRemain);
-//if(){
+var gasRemain = gasCap * (gasGauge/100);  //find out how many gallons are left
+var milRemain = gasRemain * gasEff;       //find out how many miles you still drive with gallons left in tank
+
+if(milRemain > nextGas){
     //can you make it across the desert w/current fuel?  next gas station in 200 mi.  stop, yes or no?
+    console.log("Yes, you can make it without stopping for gas!");
+}else{
+    console.log("You only have " + gasRemain + " gallons of gas in your tank, better get gas now while you can!");
+}
 
-//}
 
-//console.log("Yes, you can make it without stopping for gas!");
-//console.log("You only 'X' gallons of gas in your tank, better get gas now while you can!");
+
