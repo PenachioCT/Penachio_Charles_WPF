4c31b3d56a4d2f5cb0112467270f7024c5e0a840
Charles Penachio Jr <ctpenachio@fullsail.edu>
2014-08-19 23:36:46 -0400

    Worksheet Complete
    Finished final exercise, Group 3

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -35,21 +35,53 @@ if(milRemain > nextGas){
 console.log("");    //Spacing between Group 1 & Group 2
 
 //Group 2:  Multiple Results - Check the Login
-var userName
-var userPass
-var correctName
-var correctPass
+var correctName = "User";  //sets user name
+var correctPass = "Pass";  //sets user password
 
-if(){
+var userName = prompt("Please enter your User Name: \n (Case Sensitive)");  //asks user for the login name
+var userPass = prompt("Please enter your Password: \n (Case Sensitive)");   //asks user for the login password
 
-}else if(){
-
+if(userName === correctName && userPass === correctPass){
+    //determines if both username and pass are correct.
+    console.log("Welcome, " + correctName + "!");
+}else if(userName != correctName && userPass === correctPass){
+    //determines if username is incorrect
+    console.log("User not found.  Try again.");
 }else{
+    //determines if user password is incorrect
+    console.log("Password does not match our records.");
+}
+console.log("");    //Spacing between Group 2 & Group 3
 
+//Group 3:  Multiple Conditions - Tire Pressure I
+var tireArray = new Array();
+tireArray[0] = prompt("Please enter Front/Left tire pressure (psi):");
+tireArray[1] = prompt("Please enter Front/Right tire pressure (psi):");
+tireArray[2] = prompt("Please enter Rear/Left tire pressure (psi):");
+tireArray[3] = prompt("Please enter Rear/Right tire pressure (psi):");
+
+var tireFront;
+var tireRear;
+
+if(tireArray[0] === tireArray[1]){
+    tireFront = true;
+}else{
+    tireFront = false;
 }
 
-//console.log("Welcome, userName!");
-//console.log("User not found.  Try again.");
-//console.log("Password does not match our records.");
+if(tireArray[2] === tireArray[3]){
+    tireRear = true;
+}else{
+    tireRear = false;
+}
+
+if(tireFront === true && tireRear === true){
+    console.log("The tires pass spec!");
+}else{
+    console.log("Get your tires checked out!");
+}
+
+
+
 
 
