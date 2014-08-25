a84022a703c94664bfc3f585a8ec57907e9f661b
Charles Penachio Jr <ctpenachio@fullsail.edu>
2014-08-25 19:41:56 -0400

    While Loop
    Following Screencast:  While Loop

---
+++ Loops/js/script.js
@@ -0,0 +1,9 @@
+console.log('------ Loops ------');
+
+var b = 50;  //setups the index
+
+while (b > 0){  //checks the condition
+    consol.log(b + " kegs on the wall.");
+    b--;  //increments or decrements the index
+}
+
