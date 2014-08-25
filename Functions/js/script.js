2c54395127d3b6503b510fa2ac2f103bc77418fd
Charles Penachio Jr <ctpenachio@fullsail.edu>
2014-08-25 19:02:47 -0400

    Basic Function Structure
    Following Screencast:  Basic Function Structure

---
+++ Functions/js/script.js
@@ -0,0 +1,13 @@
+//Functions - Basic Structure
+
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
