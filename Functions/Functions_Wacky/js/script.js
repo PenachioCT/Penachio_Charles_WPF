b8964c5ce6b4243be9401a39faa2bb58538da105
Charles Penachio Jr <ctpenachio@fullsail.edu>
2014-08-28 20:29:19 -0400

    Complete Wacky Assignment
    Completed wacky assignment.

--- Functions/Functions_Wacky/js/script.js
+++ Functions/Functions_Wacky/js/script.js
@@ -6,10 +6,42 @@
  */
 
 //Let's play a shell game!
-var userName = ("Please enter your name:");
-prompt ("Hello " + userName + ", let's play a game!");
-prompt ("Find which cup the ball is hiding in...");
-var userCups = prompt ("How many cups would you like to play with?\n(Whole numbers only.  2-6 cups)");
-prompt ("Ok, we will be playing with " + userCups + " cups.");
-prompt ("Shuffling cups, please wait...");
-var userChoice = prompt ("Which cup do you think the ball is in?\n(1 - " + userCups + ")");
+var userName = prompt("Please enter your name:");  //asks user for name and stores it
+if(userName===""){  //validates user input
+    userName=("Please re-enter your name:");
+}
+console.log("This is the users name:  " + userName);  //prints user input to console
+alert("Hello " + userName + ", let's play a game!");  //message to user using input
+alert("Find which cup the ball is hiding in...");  //message to user
+var userCups = prompt ("How many cups would you like to play with?\n(Whole numbers only.  2-6 cups)");  //asks user for cups value and stores it
+if(userCups==="" || isNaN(userCups)){  //validates user input
+    userCups=prompt("Please re-enter the amount of cups using whole numbers only:  ");
+}
+console.log("This is the number of cups the user wants to use:  " + userCups);  //print user input to console
+alert("Ok, we will be playing with " + userCups + " cups.");  //message to user using input
+alert("Shuffling cups, please wait...");  //message to user
+var userChoice = prompt ("Which cup do you think the ball is in?\n(1 - " + userCups + ")");  //asks user for guess
+if(userChoice==="" || isNaN(userChoice)){  //validates users guess
+    userChoice=prompt("Please re-enter your cup choice using whole numbers only:\n(1 - " + userCups + ")");
+}
+console.log("This is the cup the user choose:  " + userChoice);  //prints users guess to console
+function findBallCup(uCups){  //creates function to find which cup the ball is in
+    var ballCup = Math.round(Math.random()*(uCups-1)+Number(1));  //calculation to find ball
+    console.log("This is the cup our ball is located in the function:  " + ballCup);  //prints ball location to console
+    return ballCup;  //returns value for use outside of function
+}
+
+var ballLocation = findBallCup(userCups);  //call to the function
+while(userChoice!=ballLocation){  //creates while loop to keep game going
+    alert("That is incorrect.  Please try again.");  //message to user
+    alert("Shuffling cups, please wait...");  //message to user
+    var userChoice = prompt ("Which cup do you think the ball is in?\n(1 - " + userCups + ")");  //asks user for another guess
+    if(userChoice==="" || isNaN(userChoice)){  //validates user guess
+        userChoice=prompt("Please re-enter your cup choice using whole numbers only:\n(1 - " + userCups + ")");
+    }
+    console.log("This is the users next guess: " + userChoice);  //prints new guess to console
+    findBallCup(userChoice);  //calls function for new ball placement on next run
+}
+alert("Congratulations " + userName + ", you won!");  //message to user
+console.log("The user has won.");  //prints win message to console
+
